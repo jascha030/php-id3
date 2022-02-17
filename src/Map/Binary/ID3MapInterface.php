7523fd5807ba0b3bb2ca5dcfdfab11fa4a14985c
Jascha030 <contact@jaschavanaalst.nl>
2022-02-17 01:43:57 +0100

    ✨ Add ID3MapInterface

--- src/Map/Binary/ID3MapInterface.php
+++ src/Map/Binary/ID3MapInterface.php
@@ -2,7 +2,15 @@
 
 namespace Jascha030\Id3\Map\Binary;
 
+use Jascha030\Id3\Tag\TagInterface;
+
 interface ID3MapInterface
 {
+    public function getIds(): array;
 
+    public function applicable(string $version): bool;
+
+    public function getOffset(): int;
+
+    public function processTag(string $tag, string $tagId): string;
 }
