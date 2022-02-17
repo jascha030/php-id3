e4a08f812268f7ddede7a17ed426d34d39510100
Jascha030 <contact@jaschavanaalst.nl>
2022-02-17 01:44:35 +0100

    🚧 Concretize some methods in Abstract class version of interface

--- src/Map/Binary/ID3MapAbstract.php
+++ src/Map/Binary/ID3MapAbstract.php
@@ -1,8 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Id3\Map\Binary;
 
-class ID3MapAbstract
+abstract class ID3MapAbstract implements ID3MapInterface
 {
+    public function __construct(private array $ids, private array $versions)
+    {
+    }
 
+    public function getIds(): array
+    {
+        return $this->ids;
+    }
+
+    final public function applicable(string $version): bool
+    {
+        return in_array($version, $this->versions, true);
+    }
+
+    final public function getOffset(): int
+    {
+        return strlen(reset($this->ids));
+    }
 }
