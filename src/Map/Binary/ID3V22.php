25e63343a9b48fdaff295dac7f2becf82a52fe34
Jascha030 <contact@jaschavanaalst.nl>
2022-02-17 01:46:14 +0100

    Add 2 default map implementations

--- src/Map/Binary/ID3V22.php
+++ src/Map/Binary/ID3V22.php
@@ -1,8 +1,34 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Id3\Map\Binary;
 
-class ID3V22
-{
+use JetBrains\PhpStorm\Pure;
 
+final class ID3V22 extends ID3MapAbstract
+{
+    public const MAP      = [
+        'Title'  => 'TT2',
+        'Album'  => 'TAL',
+        'Author' => 'TP1',
+        'Track'  => 'TRK',
+        'Year'   => 'TYE',
+        'Length' => 'TLE',
+        'Lyric'  => 'ULT'
+    ];
+
+    public const VERSIONS = ['2.0'];
+
+    #[Pure]
+    public function __construct()
+    {
+        parent::__construct(self::MAP, self::VERSIONS);
+    }
+
+    public function processTag(string $tag, string $tagId): string
+    {
+        // TODO: Implement processTag() method.
+        return '';
+    }
 }
--- src/Map/Binary/ID3V23.php
+++ src/Map/Binary/ID3V23.php
@@ -1,8 +1,33 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Id3\Map\Binary;
 
-class ID3V23
-{
+use JetBrains\PhpStorm\Pure;
 
+final class ID3V23 extends ID3MapAbstract
+{
+    public const MAP      = [
+        'Title'  => 'TIT2',
+        'Album'  => 'TALB',
+        'Author' => 'TPE1',
+        'Track'  => 'TRCK',
+        'Year'   => 'TDRC',
+        'Length' => 'TLEN',
+        'Lyric'  => 'USLT'
+    ];
+
+    public const VERSIONS = ['3.0', '4.0'];
+
+    #[Pure]
+    public function __construct()
+    {
+        parent::__construct(self::MAP, self::VERSIONS);
+    }
+
+    public function processTag(string $tag, string $tagId): string
+    {
+        // todo: implement
+    }
 }
